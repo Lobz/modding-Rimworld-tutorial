4fe5472299a64136391b8ff022098e41628a56b3
M Salles <malicsalles@gmail.com>
2021-07-30 20:05:06 -0300

    helloworlddll

---
+++ HelloWorldDLL/Source/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace HelloWorld
+{
+    [StaticConstructorOnStartup]
+    public static class HelloWorld
+    {
+        static HelloWorld() //our constructor
+        {
+            Log.Message("Hello World!"); //Outputs "Hello World!" to the dev console.
+        }
+    }
+}
